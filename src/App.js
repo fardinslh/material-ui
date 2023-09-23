c9b0930c1c52693125be41703b53e864c8546ee2
Fardin-Salehi <fardinsalehizadeh1998@gmail.com>
2023-09-23 11:33:30 +0200

    added a button in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { Button } from "@mui/material";
+
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div>
+      <Button variant="outlained">Text</Button>
     </div>
   );
 }
